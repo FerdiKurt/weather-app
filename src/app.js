5410315f7c9e45ecee3f52ce79cc1664b7236701
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2020-11-20 03:53:13 +0300

    .

--- src/app.js
+++ src/app.js
@@ -88,7 +88,7 @@ app.get('*', (req, res) => {
   })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.listen(port, () => {
   console.log('Server is up on port 3000.')
 })
